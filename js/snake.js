98ce3cccdbe3faa0b54b430c97f1535152a82305
Бат-Эрдэнэ Б <n.baterdene.goodgodking@gmail.com>
2020-07-08 00:51:44 +0800

    added snake, renderer, controller, mover, board,

--- js/snake.js
+++ js/snake.js
@@ -1,42 +1,93 @@
+// const { dir } = require('console');
+
 window.onload = function () {
   let app = new PIXI.Application({
-    width: 800,
-    height: 600,
-    backgroundColor: 0x1099bb,
+    width: 500,
+    height: 500,
     resolution: window.devicePixelRatio || 1,
   });
   document.body.appendChild(app.view);
+  const backgroundTexture = PIXI.Texture.from('assets/backgroundCell.jpg');
+  const snakeTexture = PIXI.Texture.from('assets/snakeBody.png');
+  const gameBoard = new PIXI.Container();
+  const snake = new PIXI.Container();
+  app.stage.addChild(gameBoard);
+  app.stage.addChild(snake);
 
-  const container = new PIXI.Container();
-
-  app.stage.addChild(container);
-
-  // Create a new texture
-  const texture = PIXI.Texture.from("assets/bunny.png");
-  const texture1 = PIXI.texture.
-  // Create a 5x5 grid of bunnies
-  for (let i = 0; i < 25; i++) {
-    const bunny = new PIXI.Sprite(texture);
-    bunny.width = 100;
-    bunny.height = 100;
-    bunny.anchor.set(0.5);
-    bunny.x = (i % 5) * 40;
-    bunny.y = Math.floor(i / 5) * 40;
-    container.addChild(bunny);
+  const cellSize = 10,
+    width = 50,
+    height = 50;
+  for (let i = 0; i < width * height; i++) {
+    const boardCell = new PIXI.Sprite(backgroundTexture);
+    boardCell.width = cellSize;
+    boardCell.height = cellSize;
+    boardCell.x = (i % width) * cellSize;
+    boardCell.y = Math.floor(i / width) * cellSize;
+    gameBoard.addChild(boardCell);
   }
+  let snakePos = [];
+  for (let j = 0; j < 10; j++) {
+    let curPos = [];
+    curPos[0] = 25;
+    curPos[1] = 10 + j;
+    const snakeCell = new PIXI.Sprite(snakeTexture);
+    snakeCell.width = cellSize;
+    snakeCell.height = cellSize;
+    snakeCell.x = curPos[1] * cellSize;
+    snakeCell.y = curPos[0] * cellSize;
+    snakePos.push(snakeCell);
+    gameBoard.addChild(snakeCell);
+  }
+  let directions = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+  ];
+  let direction = 1;
+  function moveSnake() {
+    for (let i = 0; i < snakePos.length - 1; i++) {
+      const snakeCell = snakePos[i];
+      snakeCell.x = snakePos[i + 1].x;
+      snakeCell.y = snakePos[i + 1].y;
+    }
+    const snakeCell = snakePos[snakePos.length - 1];
+    snakeCell.x =
+      (snakeCell.x + directions[direction][1] * cellSize + width * cellSize) %
+      (width * cellSize);
+    snakeCell.y =
+      (snakeCell.y + directions[direction][0] * cellSize + width * cellSize) %
+      (width * cellSize);
+  }
+  function startGame() {
+    setInterval(function () {
+      moveSnake();
+    }, 100);
+  }
+  startGame();
 
-  // Move container to the center
-  container.x = app.screen.width / 2;
-  container.y = app.screen.height / 2;
-
-  // Center bunny sprite in local container coordinates
-  container.pivot.x = container.width / 2;
-  container.pivot.y = container.height / 2;
-
+  document.body.onkeydown = checkKey;
+  function checkKey(e) {
+    e = e || window.event;
+    let cur;
+    if (e.keyCode == '37') {
+      cur = 2;
+    }
+    if (e.keyCode == '38') {
+      cur = 3;
+    }
+    if (e.keyCode == '39') {
+      cur = 0;
+    }
+    if (e.keyCode == '40') {
+      cur = 1;
+    }
+    if (direction % 2 != cur % 2) direction = cur;
+  }
   // Listen for animate update
   //   app.ticker.add((delta) => {
-  //     // rotate the container!
+  //     // rotate the gameBoard!
   //     // use delta to create frame-independent transform
-  //     container.rotation -= 0.01 * delta;
+  //     gameBoard.rotation -= 0.01 * delta;
   //   });
 };
