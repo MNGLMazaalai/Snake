1b2ff1327d871a5757b6bcece42e3c03e12fcd6b
Mazaalai <mazaalai@GodKings-iMac.local>
2020-07-08 12:47:06 +0800

    done!

--- js/snake.js
+++ js/snake.js
@@ -1,94 +1,138 @@
-// const { dir } = require('console');
+let app = new PIXI.Application({
+  width: 300,
+  height: 300,
+  resolution: window.devicePixelRatio || 1,
+});
 
-window.onload = function () {
-  let app = new PIXI.Application({
-    width: 500,
-    height: 500,
-    resolution: window.devicePixelRatio || 1,
-  });
-  document.body.appendChild(app.view);
-  const backgroundTexture = PIXI.Texture.from('assets/backgroundCell.jpg');
-  const snakeTexture = PIXI.Texture.from('assets/snakeBody.png');
-  const gameBoard = new PIXI.Container();
-  const snake = new PIXI.Container();
-  app.stage.addChild(gameBoard);
-  app.stage.addChild(snake);
+document.body.appendChild(app.view);
+const backgroundTexture = PIXI.Texture.from("assets/backgroundCell.jpg");
+const snakeTexture = PIXI.Texture.from("assets/snakeBody.png");
+const foodTexture = PIXI.Texture.from("assets/food.png");
+const gameBoard = new PIXI.Container();
+const snake = new PIXI.Container();
+app.stage.addChild(gameBoard);
+app.stage.addChild(snake);
 
-  const cellSize = 10,
-    width = 50,
-    height = 50;
-  for (let i = 0; i < width * height; i++) {
-    const boardCell = new PIXI.Sprite(backgroundTexture);
-    boardCell.width = cellSize;
-    boardCell.height = cellSize;
-    boardCell.x = (i % width) * cellSize;
-    boardCell.y = Math.floor(i / width) * cellSize;
-    gameBoard.addChild(boardCell);
-  }
-  let snakePos = [];
-  for (let j = 0; j < 10; j++) {
-    let curPos = [];
-    curPos[0] = 25;
-    curPos[1] = 10 + j;
-    const snakeCell = new PIXI.Sprite(snakeTexture);
-    snakeCell.width = cellSize;
-    snakeCell.height = cellSize;
-    snakeCell.x = curPos[1] * cellSize;
-    snakeCell.y = curPos[0] * cellSize;
-    snakePos.push(snakeCell);
-    gameBoard.addChild(snakeCell);
-  }
-  let directions = [
-    [0, 1],
-    [1, 0],
-    [0, -1],
-    [-1, 0],
-  ];
-  let direction = 0;
-  function moveSnake() {
-    for (let i = 0; i < snakePos.length - 1; i++) {
-      const snakeCell = snakePos[i];
-      snakeCell.x = snakePos[i + 1].x;
-      snakeCell.y = snakePos[i + 1].y;
-    }
-    const snakeCell = snakePos[snakePos.length - 1];
-    snakeCell.x =
-      (snakeCell.x + directions[direction][1] * cellSize + width * cellSize) %
-      (width * cellSize);
-    snakeCell.y =
-      (snakeCell.y + directions[direction][0] * cellSize + width * cellSize) %
-      (width * cellSize);
-  }
-  function startGame() {
-    setInterval(function () {
-      moveSnake();
-    }, 100);
-  }
-  startGame();
+const cellSize = 10,
+  width = 30,
+  height = 30;
+for (let i = 0; i < width * height; i++) {
+  const boardCell = new PIXI.Sprite(backgroundTexture);
+  boardCell.width = cellSize;
+  boardCell.height = cellSize;
+  boardCell.x = (i % width) * cellSize;
+  boardCell.y = Math.floor(i / width) * cellSize;
+  gameBoard.addChild(boardCell);
+}
+let snakePos = [];
+for (let j = 0; j < 10; j++) {
+  let curPos = [];
+  curPos[0] = width / 2;
+  curPos[1] = height / 5 + j;
+  const snakeCell = new PIXI.Sprite(snakeTexture);
+  snakeCell.width = cellSize;
+  snakeCell.height = cellSize;
+  snakeCell.x = curPos[1] * cellSize;
+  snakeCell.y = curPos[0] * cellSize;
+  snakePos.push(snakeCell);
+  gameBoard.addChild(snakeCell);
+}
+let directions = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+];
+let foodPos,
+  direction = 0,
+  nextCell,
+  done = true,
+  gameSpd = 1,
+  eaten = 0;
+let game = startGame();
+dropFood();
 
-  document.body.onkeydown = checkKey;
-  function checkKey(e) {
-    e = e || window.event;
-    let cur;
-    if (e.keyCode < 37 || e.keyCode > 40) return;
-    if (e.keyCode == '37') {
-      cur = 2;
+document.body.onkeydown = checkKey;
+
+function eat(foodPos, x, y) {
+  gameBoard.removeChild(foodPos);
+  const snakeCell = new PIXI.Sprite(snakeTexture);
+  snakeCell.width = cellSize;
+  snakeCell.height = cellSize;
+  snakeCell.x = x;
+  snakeCell.y = y;
+  snakePos.push(snakeCell);
+  gameBoard.addChild(snakeCell);
+  dropFood();
+}
+
+function moveSnake() {
+  // console.log(snakePos.length);
+  const snakeCell = snakePos[snakePos.length - 1];
+  let mod = width * cellSize;
+
+  let x = (snakeCell.x + directions[direction][1] * cellSize + mod) % mod;
+  let y = (snakeCell.y + directions[direction][0] * cellSize + mod) % mod;
+  if (y == foodPos.y && x == foodPos.x) {
+    eaten++;
+    if (eaten % 3 == 0) {
+      clearInterval(game);
+      gameSpd += 0.1;
+      game = startGame();
     }
-    if (e.keyCode == '38') {
-      cur = 3;
-    }
-    if (e.keyCode == '39') {
-      cur = 0;
-    }
-    if (e.keyCode == '40') {
-      cur = 1;
-    }
-    if (direction % 2 != cur % 2) direction = cur;
+
+    eat(foodPos, x, y);
+    done = true;
+    return;
   }
-  // Listen for animate update
-  //   app.ticker.add((delta) => {
-  //     // rotate the gameBoard!
-  //     // use delta to create frame-independent transform
-  //     gameBoard.rotation -= 0.01 * delta;
-  //   });
-};
+  for (let i = 0; i < snakePos.length - 1; i++) {
+    const snakeCell = snakePos[i];
+    if (snakeCell.x == x && snakeCell.y == y) return die();
+  }
+  for (let i = 0; i < snakePos.length - 1; i++) {
+    const snakeCell = snakePos[i];
+    snakeCell.x = snakePos[i + 1].x;
+    snakeCell.y = snakePos[i + 1].y;
+  }
+  snakeCell.x = (snakeCell.x + directions[direction][1] * cellSize + mod) % mod;
+  snakeCell.y = (snakeCell.y + directions[direction][0] * cellSize + mod) % mod;
+  done = true;
+}
+function startGame() {
+  return setInterval(function () {
+    moveSnake();
+  }, 100 / gameSpd);
+}
+function die() {
+  alert("Game over!");
+  clearInterval(game);
+}
+function checkKey(e) {
+  e = e || window.event;
+  let cur;
+  if (e.keyCode < 37 || e.keyCode > 40 || !done) return;
+  done = false;
+  if (e.keyCode == "37") cur = 2;
+  if (e.keyCode == "38") cur = 3;
+  if (e.keyCode == "39") cur = 0;
+  if (e.keyCode == "40") cur = 1;
+  if (direction % 2 != cur % 2) direction = cur;
+}
+function dropFood() {
+  let x = Math.floor(Math.random() * width) * cellSize;
+  let y = Math.floor(Math.random() * height) * cellSize;
+  let empty = true;
+  for (let i = 0; i < snakePos.length; i++) {
+    const snakeCell = snakePos[i];
+    if (x == snakeCell.x && y == snakeCell.y) empty = false;
+  }
+  if (empty) {
+    const food = new PIXI.Sprite(foodTexture);
+    food.width = cellSize;
+    food.height = cellSize;
+    food.x = x;
+    food.y = y;
+    gameBoard.addChild(food);
+    foodPos = food;
+  } else dropFood();
+}
